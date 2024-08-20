4de76ed9df80b96f9dd219acd42335c7d2435231
Daniil Krizhanovskyi <daniil.krizhanovskyi@hotmail.com>
2024-08-20 16:18:17 +0200

    Delete tools/error_analysis.py

--- tools/error_analysis.py
+++
@@ -1,7 +0,0 @@
-# Utility functions for quantum error analysis
-from qiskit.visualization import plot_error_map
-
-def analyze_errors(backend):
-    """Analyze and plot the error map of a quantum backend"""
-    error_map = backend.properties().to_dict()['qubits']
-    plot_error_map(backend)
